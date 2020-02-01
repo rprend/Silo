3c62b4076db73bf84320bd9f9a1bd4177ef0efdf
beezow <zbeezow@gmail.com>
2020-01-31 23:04:51 -0500

    generic chart

--- silo_app/app/src/main/java/com/silo/silo_app/MainActivity.java
+++ silo_app/app/src/main/java/com/silo/silo_app/MainActivity.java
@@ -3,6 +3,12 @@ package com.silo.silo_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            entries.add(new Entry(i, i*i));
+        }
+        //charts -- mult lines //2560X1312
+        SiloLineChart chart = new SiloLineChart("test", "this is test data", entries, (LinearLayout) findViewById(R.id.graph_root), this);
     }
 }
